9dee0508508d036adf404f2f3907616a07ed9051
tuannt <tuanbaros@yahoo.com.vn>
2017-10-20 14:21:18 +0700

    T04 - Saving Data

---
+++ app/src/main/java/com/simple/training/data/FeedReaderContract.java
@@ -0,0 +1,20 @@
+package com.simple.training.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by FRAMGIA\nguyen.thanh.tuan on 10/20/17.
+ */
+
+public class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
